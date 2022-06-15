9649f422e5de3f7f88fe0c6680c39f035dcfe970
erol <hurkanrol15@gmail.com>
2022-06-15 17:07:10 +0200

    added post delete

--- src/blogapp/urls.py
+++ src/blogapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import post_detail, post_list, post_create, post_update
+from .views import post_delete, post_detail, post_list, post_create, post_update
 
 
 app_name='blogapp'
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", post_list, name='list'),
     path("create/", post_create, name='create'),
     path("<str:slug>/", post_detail, name='detail'),
-    path("<str:slug>/update", post_update, name='update')
+    path("<str:slug>/update", post_update, name='update'),
+    path("<str:slug>/delete", post_delete, name='delete')
 ]
