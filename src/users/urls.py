ccb3c2cea145cc42f5d3710ff231d3322495eca1
erol <hurkanrol15@gmail.com>
2022-08-01 18:13:25 +0200

    added login and logout pages

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,3 +1,4 @@
+from re import template
 from django import urls
 from django.urls import path
 from django.contrib.auth import views as auth_views
@@ -5,6 +6,7 @@ from .views import register,profile
 
 urlpatterns = [
     path("register/", register, name="register"),
-    path("profile/", profile, name="profile")
-
+    path("profile/", profile, name="profile"),
+    path("login/", auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(template_name="users/logout.html"), name="logout")
 ]
